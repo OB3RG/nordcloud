937a5de792b53b68106f72083515273854437d38
Pantelis <tampantelis@gmail.com>
2019-06-24 13:50:01 +0300

    add loop that prints the results

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-#Problem solving
+# Problem solving
 
 Write a program that solves the most suitable (with most power) link station for a device at given
 point [x,y].
--- linkStationPower.js
+++ linkStationPower.js
@@ -10,7 +10,7 @@ const stations = [
   [0, 0, 10],
   [20, 20, 5],
   [10, 0, 12]
-]
+];
 
 
 const findDistance = (pointA, pointB) => {
@@ -19,12 +19,28 @@ const findDistance = (pointA, pointB) => {
 
 const findPower = (point , station) => {
   const distance = findDistance(point, station);
-
-  if (distance > station ) {
+  if (distance > station[2] ) {
     return 0;
   } else{
   return Math.pow(Math.abs(station[2] - distance) ,2);
   }
-}
+};
 
-console.log(findPower(points[2], stations[1]));
+for(let i=0; i<points.length; i++){
+  let bestStation = [0,0,0];
+  for(let j=0; j < stations.length; j++){
+    let power = findPower(points[i],stations[j]);
+    if( power > bestStation[0]){
+      bestStation[0] = power ;
+      bestStation[1] = stations[j][0];
+      bestStation[2] = stations[j][1];   
+    }
+  };
+
+  if (bestStation[0] == 0) {
+    console.log("No link station within reach for point " + points[i][0] +"," + points[i][1]);
+  } else {
+    console.log("Best link station for point " + points[i][0]  + "," + points[i][1] + " is " + bestStation[1] + "," + bestStation[2] + " with power " + bestStation[0]);
+  };
+
+};
